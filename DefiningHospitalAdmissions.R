e9ae83b257491e2138e08f39773e7111c94ca7cc
jocelynfriday <jocelynfriday@yahoo.com>
2025-05-28 16:28:26 +0100

    Update DefiningHospitalAdmissions.R
    Change to camel case

--- DefiningHospitalAdmissions.R
+++ DefiningHospitalAdmissions.R
@@ -14,7 +14,7 @@ library(data.table)
 smr01All <- fread("SMR01.csv")
 
 #### Step 1a: If the SMR01 data does not have a unique row ID, add one
-smr01All[, row_ID := .I]
+smr01All[, rowID := .I]
 
 ### Step 2: Update data types & subset to necessary columns
 smr01All$SafeHavenID <- as.factor(smr01All$SafeHavenID)
@@ -22,7 +22,7 @@ smr01All$ADMDATE <- as.Date(smr01All$ADMDATE)
 smr01All$DISDATE <- as.Date(smr01All$DISDATE)
 
 ##### Subset away to only pertinent information 
-smr01 <- smr01All[, c("SafeHavenID", "ADMDATE", "DISDATE", "row_ID")]
+smr01 <- smr01All[, c("SafeHavenID", "ADMDATE", "DISDATE", "rowID")]
 
 ###Step 3: Order smr01 by SafeHavenID, admission date (ADMDATE), and discharge date (DISDATE) in ascending order
 setorder(smr01, SafeHavenID, ADMDATE, DISDATE)
@@ -31,34 +31,34 @@ setorder(smr01, SafeHavenID, ADMDATE, DISDATE)
 #### 1 indicates the start of a new admission series, while 0 indicates a continuation of the admission
 smr01[, series := 1]
 
-### Step 5: Identify row_IDs of nested episodes of care
-rows_to_flag <- smr01[, {
+### Step 5: Identify rowIDs of nested episodes of care
+rowsToFlag <- smr01[, {
   # Define variables
   flagged <- integer(0)
   ## Most recent, maximum, discharge date seen so far set to the first by default
-  max_disdate <- DISDATE[1]
+  maxDisdate <- DISDATE[1]
   
   # If a SafeHavenID has more than 1 episode of care, enter the if statement
   if (.N > 1){
     
     # For all episodes of care:
     for (i in 2L:.N){
-      # If the current admission date (ADMDATE) is within 1 day of the maximum discharge date (max_disdate), store the row_ID with 0
-      if(!is.na(ADMDATE[i]) && ! is.na(max_disdate) && as.numeric(ADMDATE[i] -max_disdate) <= 1){
-        flagged <- c(flagged, row_ID[i])
+      # If the current admission date (ADMDATE) is within 1 day of the maximum discharge date (maxDisdate), store the rowID with 0
+      if(!is.na(ADMDATE[i]) && ! is.na(maxDisdate) && as.numeric(ADMDATE[i] -maxDisdate) <= 1){
+        flagged <- c(flagged, rowID[i])
       }
-      # Also, if the current discharge date (DISDATE) is greater than the maximum seen discharge date (max_disdate), update max_disdate
-      if(!is.na(DISDATE[i]) && DISDATE[i] > max_disdate) {
-        max_disdate <- DISDATE[i]
+      # Also, if the current discharge date (DISDATE) is greater than the maximum seen discharge date (maxDisdate), update maxDisdate
+      if(!is.na(DISDATE[i]) && DISDATE[i] > maxDisdate) {
+        maxDisdate <- DISDATE[i]
       }
     }
     
   }
-  .(row_ID = flagged)
+  .(rowID = flagged)
 }, by = SafeHavenID]
 
-### Step 6: Update series to 0 where the row_ID was identified during Step 4
-smr01[row_ID %in% rows_to_flag$row_ID, series := 0]
+### Step 6: Update series to 0 where the rowID was identified during Step 4
+smr01[rowID %in% rowsToFlag$rowID, series := 0]
 
 ### Step 7: Create and assign a unique hospStayID per continuous admission
 #### Increment hospStayID at every 1, and remain the same for every 0 in the series 
@@ -66,5 +66,5 @@ smr01[, hospStayID := 0L]
 smr01[, hospStayID := cumsum(series == 1)]
 
 ### Step 8: Merge hospStayID back onto smr01
-smr01 <- smr01[,c("row_ID", "hospStayID")]
-smr01All <- merge(smr01All, smr01, by= "row_ID", all= TRUE)
+smr01 <- smr01[,c("rowID", "hospStayID")]
+smr01All <- merge(smr01All, smr01, by= "rowID", all= TRUE)
